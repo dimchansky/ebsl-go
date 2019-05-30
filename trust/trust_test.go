3c6265ddb197266599f1e52a8c0e49e6aa4ffdd6
Dmitrij Koniajev <dimchansky@gmail.com>
2019-05-30 18:31:03 +0300

    refactoring/expression evaluator implemented

--- trust/trust_test.go
+++ trust/trust_test.go
@@ -9,23 +9,26 @@ import (
 	"github.com/dimchansky/ebsl-go/trust"
 )
 
-type equationStringer struct {
+type expressionStringer struct {
 	strings.Builder
 }
 
-func (s *equationStringer) VisitFullUncertainty() {
+func (s *expressionStringer) VisitFullUncertainty() {
 	_, _ = s.WriteString("U")
 }
 
-func (s *equationStringer) VisitDiscountingRule(r trust.Link, a trust.Link) {
+func (s *expressionStringer) VisitDiscountingRule(r trust.Link, a trust.Link) {
 	_, _ = s.WriteString(fmt.Sprintf("(%v ⊠ %v)", rToString(r), aToString(a)))
 }
 
-func (s *equationStringer) VisitDirectReferralTrust(a trust.Link) {
+func (s *expressionStringer) VisitDirectReferralTrust(a trust.Link) {
 	_, _ = s.WriteString(aToString(a))
 }
 
-func (s *equationStringer) VisitConsensusList(index int, equation trust.Equation) {
+func (s *expressionStringer) VisitConsensusListStart(count int) {
+}
+
+func (s *expressionStringer) VisitConsensusList(index int, equation trust.Expression) {
 	eqStr := eqToString(equation)
 
 	if index == 0 {
@@ -36,15 +39,18 @@ func (s *equationStringer) VisitConsensusList(index int, equation trust.Equation
 	}
 }
 
+func (s *expressionStringer) VisitConsensusListEnd() {
+}
+
 func rToString(r trust.Link) string { return fmt.Sprintf("R[%v,%v]", r.From, r.To) }
 func aToString(a trust.Link) string { return fmt.Sprintf("A[%v,%v]", a.From, a.To) }
-func eqToString(eq trust.Equation) string {
-	s := &equationStringer{}
-	eq.Accept(s)
+func eqToString(expr trust.Expression) string {
+	s := &expressionStringer{}
+	expr.Accept(s)
 	return s.String()
 }
 func frtEqToString(eq *trust.FinalReferralTrustEquation) string {
-	return rToString(eq.R) + " = " + eqToString(eq.Equation)
+	return rToString(eq.R) + " = " + eqToString(eq.Expression)
 }
 
 func logEquations(t *testing.T, eqs []*trust.FinalReferralTrustEquation) {
@@ -56,19 +62,19 @@ func logEquations(t *testing.T, eqs []*trust.FinalReferralTrustEquation) {
 func TestExample(t *testing.T) {
 	c := uint64(2)
 
-	logEquations(t, trust.DirectReferralEvidence{
+	a := trust.DirectReferralEvidence{
 		trust.Link{From: 1, To: 2}: evidence.New(1, 1),
 		trust.Link{From: 2, To: 3}: evidence.New(1, 1),
 		trust.Link{From: 3, To: 2}: evidence.New(1, 1),
-	}.
-		ToDirectReferralOpinion(c).
-		CreateFinalReferralTrustEquations())
+	}.ToDirectReferralOpinion(c)
+
+	logEquations(t, trust.CreateFinalReferralTrustEquations(a))
 }
 
 func TestExample2(t *testing.T) {
 	c := uint64(2)
 
-	logEquations(t, trust.DirectReferralEvidence{
+	a := trust.DirectReferralEvidence{
 		trust.Link{From: 1, To: 2}: evidence.New(1, 1),
 		trust.Link{From: 2, To: 3}: evidence.New(1, 1),
 		trust.Link{From: 3, To: 4}: evidence.New(1, 1),
@@ -77,7 +83,7 @@ func TestExample2(t *testing.T) {
 		trust.Link{From: 4, To: 6}: evidence.New(1, 1),
 		trust.Link{From: 5, To: 6}: evidence.New(1, 1),
 		trust.Link{From: 6, To: 7}: evidence.New(1, 1),
-	}.
-		ToDirectReferralOpinion(c).
-		CreateFinalReferralTrustEquations())
+	}.ToDirectReferralOpinion(c)
+
+	logEquations(t, trust.CreateFinalReferralTrustEquations(a))
 }
