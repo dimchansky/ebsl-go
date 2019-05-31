6827f9b55675549bc2f14d26e379b08ea3a0e5cc
Dmitrij Koniajev <dimchansky@gmail.com>
2019-05-31 15:46:21 +0300

    commenting opinion package

--- opinion/opinion.go
+++ opinion/opinion.go
@@ -6,20 +6,26 @@ import (
 	"github.com/dimchansky/ebsl-go/evidence"
 )
 
+// Type of opinion
 type Type struct {
 	B, D, U float64
 }
 
+// String implements fmt.Stringer
 func (x *Type) String() string {
 	return fmt.Sprintf("{B: %v, D: %v, U: %v}", x.B, x.D, x.U)
 }
 
+// New creates new instance of opinion
 func New(b, d, u float64) *Type {
 	return &Type{B: b, D: d, U: u}
 }
 
+// FromEvidence converts evidence to opinion using `c` as soft threshold/"unit" of evidence (must be positive number).
 func FromEvidence(c uint64, e *evidence.Type) *Type { return new(Type).FromEvidence(c, e) }
 
+// FromEvidence converts evidence and updates opinion using `c` as soft threshold/"unit" of evidence (must be positive number).
+// Method returns updated value.
 func (x *Type) FromEvidence(c uint64, e *evidence.Type) *Type {
 	k := float64(c) + e.P + e.N
 	x.B = e.P / k
@@ -28,20 +34,24 @@ func (x *Type) FromEvidence(c uint64, e *evidence.Type) *Type {
 	return x
 }
 
+// ToEvidence converts opinion to evidence using `c` as soft threshold/"unit" of evidence (must be positive number).
 func (x *Type) ToEvidence(c uint64) *evidence.Type {
 	p := float64(c) * x.B / x.U
 	n := float64(c) * x.D / x.U
 	return evidence.New(p, n)
 }
 
+// FullBelief returns full belief opinion
 func FullBelief() *Type {
 	return &Type{1, 0, 0}
 }
 
+// FullDisbelief returns full disbelief opinion
 func FullDisbelief() *Type {
 	return &Type{0, 1, 0}
 }
 
+// FullUncertainty returns full uncertainty opinion
 func FullUncertainty() *Type {
 	return &Type{0, 0, 1}
 }
