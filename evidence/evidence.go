5efbc3f5102e7219f4c6d39e4a59d9c9c967eb4b
Dmitrij Koniajev <dimchansky@gmail.com>
2019-05-31 15:40:10 +0300

    commenting evidence package

--- evidence/evidence.go
+++ evidence/evidence.go
@@ -2,14 +2,17 @@ package evidence
 
 import "fmt"
 
+// Type of evidence
 type Type struct {
 	P, N float64
 }
 
+// String implements fmt.Stringer
 func (e *Type) String() string {
 	return fmt.Sprintf("{P: %v, N: %v}", e.P, e.N)
 }
 
+// New creates new instance of evidence
 func New(p, n float64) *Type {
 	return &Type{P: p, N: n}
 }
