f7b262ff45e7d17d396d768dc0558148b07f2eea
Dmitrij Koniajev <dimchansky@gmail.com>
2019-07-31 13:42:17 +0300

    All examples removed, because they duplicate solver tests
    Added benchmark for full cycle: create equation and run one epoch iteration

--- trust/trust_test.go
+++ trust/trust_test.go
@@ -2,7 +2,6 @@ package trust_test
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/dimchansky/ebsl-go/evidence"
@@ -12,222 +11,38 @@ import (
 	"github.com/dimchansky/ebsl-go/trust/equations/solver"
 )
 
-func TestExample(t *testing.T) {
-	c := uint64(2)
+func BenchmarkCreateEquationsAndSolveThemInOneEpoch(b *testing.B) {
+	for _, nodes := range []uint64{
+		10,
+		20,
+		50,
+		100,
+	} {
+		b.Run(fmt.Sprintf("%v nodes", nodes), func(b *testing.B) {
+			dro := make(trust.DirectReferralOpinion, nodes*(nodes-1))
+			for i := uint64(1); i <= nodes; i++ {
+				for j := uint64(1); j <= nodes; j++ {
+					if i != j {
+						dro[trust.Link{From: i, To: j}] = opinion.FromEvidence(2, evidence.New(1, 1))
+					}
+				}
+			}
 
-	a := trust.DirectReferralEvidence{
-		trust.Link{From: 1, To: 2}: evidence.New(2, 2),
-		trust.Link{From: 2, To: 3}: evidence.New(2, 2),
-		trust.Link{From: 3, To: 2}: evidence.New(2, 2),
-	}.ToDirectReferralOpinion(c)
+			b.ReportAllocs()
+			b.ResetTimer()
 
-	eqs := equations.CreateFinalReferralTrustEquations(a)
-	logEquations(t, eqs)
+			for i := 0; i < b.N; i++ {
+				eqs := equations.CreateFinalReferralTrustEquations(dro)
+				context := equations.NewDefaultFinalReferralTrustEquationContext(dro)
 
-	context := equations.NewDefaultFinalReferralTrustEquationContext(a)
-
-	if err := solver.SolveFinalReferralTrustEquations(
-		context,
-		eqs,
-		solver.UseOnEpochEndCallback(func(epoch uint, aggregatedDistance float64) error {
-			t.Logf("Epoch %v error: %v\n", epoch, aggregatedDistance)
-			return nil
-		}),
-	); err != nil {
-		t.Fatal(err)
-	}
-
-	logDiscountValues(t, context)
-}
-
-func TestExample2(t *testing.T) {
-	c := uint64(2)
-
-	a := trust.DirectReferralEvidence{
-		trust.Link{From: 1, To: 2}: evidence.New(2, 2),
-		trust.Link{From: 2, To: 3}: evidence.New(2, 2),
-		trust.Link{From: 3, To: 4}: evidence.New(2, 2),
-		trust.Link{From: 4, To: 1}: evidence.New(2, 2),
-	}.ToDirectReferralOpinion(c)
-
-	eqs := equations.CreateFinalReferralTrustEquations(a)
-	logEquations(t, eqs)
-
-	context := equations.NewDefaultFinalReferralTrustEquationContext(a)
-
-	if err := solver.SolveFinalReferralTrustEquations(
-		context,
-		eqs,
-		solver.UseOnEpochEndCallback(func(epoch uint, aggregatedDistance float64) error {
-			t.Logf("Epoch %v error: %v\n", epoch, aggregatedDistance)
-			return nil
-		}),
-	); err != nil {
-		t.Fatal(err)
-	}
-
-	logDiscountValues(t, context)
-}
-
-func TestExample3(t *testing.T) {
-	c := uint64(2)
-
-	a := trust.DirectReferralEvidence{
-		trust.Link{From: 1, To: 2}: evidence.New(400, 300),
-		trust.Link{From: 2, To: 3}: evidence.New(10, 5),
-		trust.Link{From: 3, To: 4}: evidence.New(500, 0),
-		trust.Link{From: 3, To: 5}: evidence.New(500, 0),
-		trust.Link{From: 4, To: 5}: evidence.New(500, 0),
-		trust.Link{From: 4, To: 6}: evidence.New(500, 0),
-		trust.Link{From: 5, To: 6}: evidence.New(500, 0),
-		trust.Link{From: 6, To: 7}: evidence.New(5, 5),
-	}.ToDirectReferralOpinion(c)
-
-	eqs := equations.CreateFinalReferralTrustEquations(a)
-	logEquations(t, eqs)
-
-	context := equations.NewDefaultFinalReferralTrustEquationContext(a)
-
-	if err := solver.SolveFinalReferralTrustEquations(
-		context,
-		eqs,
-		solver.UseOnEpochEndCallback(func(epoch uint, aggregatedDistance float64) error {
-			t.Logf("Epoch %v error: %v\n", epoch, aggregatedDistance)
-			return nil
-		}),
-	); err != nil {
-		t.Fatal(err)
-	}
-
-	logDiscountValues(t, context)
-
-	f1P := equations.EvaluateFinalFunctionalTrust(context, 1,
-		trust.DirectFunctionalTrust{
-			7: opinion.FromEvidence(c, evidence.New(10, 90)),
+				if err := solver.SolveFinalReferralTrustEquations(
+					context,
+					eqs,
+					solver.UseMaxEpochs(1),
+				); err != nil {
+					b.Fatal(err)
+				}
+			}
 		})
-	t.Logf("f1P: %v", f1P)
-}
-
-func TestExample4(t *testing.T) {
-	c := uint64(2)
-
-	a := trust.DirectReferralEvidence{
-		trust.Link{From: 1, To: 2}: evidence.New(2, 0),
-		trust.Link{From: 2, To: 3}: evidence.New(2, 0),
-		trust.Link{From: 3, To: 4}: evidence.New(2, 0),
-	}.ToDirectReferralOpinion(c)
-
-	eqs := equations.CreateFinalReferralTrustEquations(a)
-	logEquations(t, eqs)
-
-	context := equations.NewDefaultFinalReferralTrustEquationContext(a)
-
-	if err := solver.SolveFinalReferralTrustEquations(
-		context,
-		eqs,
-		solver.UseOnEpochEndCallback(func(epoch uint, aggregatedDistance float64) error {
-			t.Logf("Epoch %v error: %v\n", epoch, aggregatedDistance)
-			return nil
-		}),
-	); err != nil {
-		t.Fatal(err)
 	}
-
-	logDiscountValues(t, context)
-}
-
-func TestExample5(t *testing.T) {
-	c := uint64(2)
-
-	a := trust.DirectReferralEvidence{
-		trust.Link{From: 1, To: 2}: evidence.New(2, 0),
-		trust.Link{From: 2, To: 4}: evidence.New(1, 0),
-	}.ToDirectReferralOpinion(c)
-
-	eqs := equations.CreateFinalReferralTrustEquations(a)
-	logEquations(t, eqs)
-
-	context := equations.NewDefaultFinalReferralTrustEquationContext(a)
-
-	if err := solver.SolveFinalReferralTrustEquations(
-		context,
-		eqs,
-		solver.UseOnEpochEndCallback(func(epoch uint, aggregatedDistance float64) error {
-			t.Logf("Epoch %v error: %v\n", epoch, aggregatedDistance)
-			return nil
-		}),
-	); err != nil {
-		t.Fatal(err)
-	}
-
-	logDiscountValues(t, context)
-}
-
-func logDiscountValues(t *testing.T, context *equations.DefaultFinalReferralTrustEquationContext) {
-	for key, value := range context.FinalReferralTrust {
-		t.Logf("g[R[%v,%v]] = g[%v] = %v\n", key.From, key.To, value, context.GetDiscount(value))
-	}
-}
-
-func logEquations(t *testing.T, eqs []*equations.FinalReferralTrustEquation) {
-	for _, eq := range eqs {
-		t.Log(frtEqToString(eq))
-	}
-}
-
-func rToString(r trust.Link) string { return fmt.Sprintf("R[%v,%v]", r.From, r.To) }
-
-func aToString(a trust.Link) string { return fmt.Sprintf("A[%v,%v]", a.From, a.To) }
-
-func eqToString(expr equations.FinalReferralTrustExpression) string {
-	s := &expressionStringer{}
-	if err := expr.Accept(s); err != nil {
-		return err.Error()
-	}
-	return s.String()
-}
-
-func frtEqToString(eq *equations.FinalReferralTrustEquation) string {
-	return rToString(eq.R) + " = " + eqToString(eq.Expression)
-}
-
-type expressionStringer struct {
-	strings.Builder
-}
-
-func (s *expressionStringer) VisitFullUncertainty() (err error) {
-	_, err = s.WriteString("U")
-	return
-}
-
-func (s *expressionStringer) VisitDiscountingRule(r trust.Link, a trust.Link) (err error) {
-	_, err = s.WriteString(fmt.Sprintf("(%v ⊠ %v)", rToString(r), aToString(a)))
-	return
-}
-
-func (s *expressionStringer) VisitDirectReferralTrust(a trust.Link) (err error) {
-	_, err = s.WriteString(aToString(a))
-	return
-}
-
-func (s *expressionStringer) VisitConsensusListStart(count int) (err error) {
-	return nil
-}
-
-func (s *expressionStringer) VisitConsensusList(index int, equation equations.FinalReferralTrustExpression) (err error) {
-	eqStr := eqToString(equation)
-
-	if index == 0 {
-		_, err = s.WriteString(eqStr)
-	} else {
-		if _, err = s.WriteString(" ⊕ "); err != nil {
-			return
-		}
-		_, err = s.WriteString(eqStr)
-	}
-	return
-}
-
-func (s *expressionStringer) VisitConsensusListEnd() (err error) {
-	return nil
 }
